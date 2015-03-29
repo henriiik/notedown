fbedbe2537d828fd43f600c83d37c788ff0b9bad
Henrik Rosén <henriiik@gmail.com>
2015-03-29 12:29:18 +0200

    update server script

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var httpServer = require('http-server');
 
-var port = process.env.OPENSHIFT_NODEJS_PORT ? process.env.OPENSHIFT_NODEJS_PORT : 9000;
-var ip = process.env.OPENSHIFT_NODEJS_IP ? process.env.OPENSHIFT_NODEJS_IP : '0.0.0.0';
+var port = process.env.OPENSHIFT_NODEJS_PORT || 9000;
+var ip = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+var path = './build';
 
-console.log(port, ip);
+console.log(port, ip, path);
 
 var server = httpServer.createServer({
-    root: 'build'
+    root: path
 });
 
 server.listen(9000, '0.0.0.0', function () {
