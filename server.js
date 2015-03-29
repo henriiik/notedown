11cf7077f38959dbe0f65b0f9f7a159536a9e921
Henrik Rosén <henriiik@gmail.com>
2015-03-29 12:01:07 +0200

    add debug message

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var httpServer = require('http-server');
 var port = process.env.OPENSHIFT_NODEJS_PORT ? process.env.OPENSHIFT_NODEJS_PORT : 9000;
 var ip = process.env.OPENSHIFT_NODEJS_IP ? process.env.OPENSHIFT_NODEJS_IP : '0.0.0.0';
 
+console.log(port, ip);
+
 var server = httpServer.createServer({
     root: 'build'
 });
