c6152ef73acaa3740540a7d52c9db77d80f1c128
Henrik Rosén <henriiik@gmail.com>
2015-03-31 19:39:27 +0200

    add build step that builds all the modules!
    - a bit tricky getting my head wrapped around all the options for the builder
    - can't use same index.html for both dev and production since dev requires system.js in separate file
    - loads a lot faster!

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
-/*eslint "strict": [2, "global"], "maxlen": [2, 100, 4] */
+/*eslint "strict": 0, "max-len": [2, 100, 4] */
 'use strict';
 
 var gulp = require('gulp');
 var del = require('del');
+var Builder = require('systemjs-builder');
 
 var paths = {
-    app: [
-        'src/**/*'
+    html: [
+        'src/**/*.html'
+    ],
+    scripts: [
+        'src/**/*.js'
     ],
     libs: [
         'node_modules/angular/angular.js',
@@ -21,19 +25,51 @@ gulp.task('clean', function (callBack) {
     del(paths.build, callBack);
 });
 
-gulp.task('app', function () {
-    return gulp.src(paths.app)
+gulp.task('copy:html', function () {
+    return gulp.src(paths.html)
         .pipe(gulp.dest(paths.build));
 });
 
-gulp.task('libs', function () {
+gulp.task('copy:scripts', function () {
+    return gulp.src(paths.sripts)
+        .pipe(gulp.dest(paths.build));
+});
+
+gulp.task('copy:libs', function () {
     return gulp.src(paths.libs)
         .pipe(gulp.dest(paths.build));
 });
 
-gulp.task('build', ['app', 'libs']);
+gulp.task('copy', ['copy:scripts', 'copy:html', 'copy:libs']);
 
-gulp.task('watch', ['build'], function () {
-    gulp.watch(paths.app, ['app']);
-    gulp.watch(paths.libs, ['libs']);
+gulp.task('watch', ['copy'], function () {
+    gulp.watch(paths.scripts, ['copy:scripts']);
+    gulp.watch(paths.html, ['copy:html']);
+    gulp.watch(paths.libs, ['copy:libs']);
+});
+
+gulp.task('build', ['copy:html'], function () {
+    return new Builder({
+            baseURL: 'src/',
+            paths: {
+                'angular': '../node_modules/angular/angular.js',
+                'traceur': '../node_modules/traceur/bin/traceur'
+            },
+            meta: {
+                'angular': {
+                    format: 'global',
+                    exports: 'angular'
+                }
+            }
+        })
+        .buildSFX('app', 'build/bootstrap.js', {
+            minify: true
+        })
+        .then(function () {
+            console.log('Build complete');
+        })
+        .catch(function (err) {
+            console.log('Build error');
+            console.log(err);
+        });
 });
