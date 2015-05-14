277cfb37f9192942cb464b8003ca9a8ae731e381
Henrik Rosén <henrik@Henriks-MacBook-Pro.local>
2015-05-14 10:05:26 +0200

    improve e2e test

--- e2e/notedown.spec.js
+++ e2e/notedown.spec.js
@@ -1,17 +1,27 @@
 /* globals browser, element, by */
 describe('Notedown', function () {
+    function Page() {
+        var page = this;
+
+        page.get = function () {
+            browser.get('http://localhost:9000')
+        }
+
+        page.signInButton = element(by.buttonText('Sign in with Google'))
+
+        return page;
+    }
+    var page;
     beforeEach(function () {
-        browser.get('http://localhost:9000');
+        page = new Page();
+        page.get();
     });
 
     it('should have a title', function () {
         expect(browser.getTitle()).toEqual('Notedown!');
     });
 
-    it('should update on button click', function () {
-        var helloElement = element(by.binding('vm.hello'));
-        expect(helloElement.getText()).toEqual('NoteDown!');
-        element(by.buttonText('update!')).click();
-        expect(helloElement.getText()).toEqual('test! NoteDown!');
+    it('should have sign in button', function () {
+        expect(page.signInButton.isPresent()).toBe(true);
     });
 });
